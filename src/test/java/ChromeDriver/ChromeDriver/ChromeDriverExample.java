e66ed5f446c07c2e4820af813f8d4299e748e1fb
Cyron <lionryuugeki@gmail.com>
2018-12-15 18:49:44 -0300

    update

--- src/test/java/ChromeDriver/ChromeDriver/ChromeDriverExample.java
+++ src/test/java/ChromeDriver/ChromeDriver/ChromeDriverExample.java
@@ -1,4 +1,9 @@
 package ChromeDriver.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -6,15 +11,14 @@ import java.nio.file.Paths;
 
 public class ChromeDriverExample {
 
-    public static void main(String[] args) throws Exception {/*
+    public static void main(String[] args) throws Exception {
         String path = Paths.get(System.getProperty("user.dir"), "src/main/resources/drivers/chromedriver.exe").toString();
         System.setProperty("webdriver.chrome.driver", path);
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.cl/");
         Thread.sleep(5000);
         driver.close();
-        driver.quit();*/
-    	System.out.println("hola");
+        driver.quit();
     }
 
 }
